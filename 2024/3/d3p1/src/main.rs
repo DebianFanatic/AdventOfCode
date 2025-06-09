9f8e06f674d6b1fc8ed93a3e250f7c4a6decf0d7
Kent West <kent.west@gmail.com>
2025-06-09 18:37:10 -0500

    Added signature to Day 3

--- 2024/3/d3p1/src/main.rs
+++ 2024/3/d3p1/src/main.rs
@@ -6,6 +6,10 @@ use std::fs::*;
 const INPUT_FILE: &str = "src/input.dat";
 
 fn main() {
+    println!("Advent of Code");
+    println!("Day 3, Part 1");
+    println!("Kent West - 9June.2025");
+
     // Get data from input file, as a String.
     let err_msg = format!("Error reading \"{}\".", INPUT_FILE);
     let data_string: String = read_to_string(INPUT_FILE).expect(&err_msg);
