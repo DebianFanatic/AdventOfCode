b2a910e6c01296137e4bd9dc3c672792d182a47f
Kent West <kent.west@gmail.com>
2025-06-09 21:09:05 -0500

    Just realized I've got the wrong file naming.

--- 2024/3/d3p2/src/main.rs
+++ 2024/3/d3p2/src/main.rs
@@ -7,17 +7,20 @@ fn main() {
     println!("Kent West - 9June.2025");
 
     // Get data.
-    let data = read_to_string("src/data.sam").expect("Error reading input data file.");
+    let data = read_to_string("src/data.txt").expect("Error reading input data file.");
 
     // Define the regex for which we're searching.
     let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    let mut total = 0;
 
-    println!("{}", data);
-
-    // Find all matches for that regex and put them in a vector,
-    // stripping away all but the "number,number" pairs in the match.
+    // Find all matches for that regex.
     for mat in re.find_iter(&data) {
+        // For each match, strip all but the "number,number" pair portion
         let pair = &mat.as_str()[4..(&mat.as_str().len() - 1)];
-        println!("{}", pair);
+        let pair: Vec<&str> = pair.trim().split(',').collect();
+        let x: i32 = pair[0].parse().expect("Error converting ASCII to i32");
+        let y: i32 = pair[1].parse().expect("Error converting ASCII to i32");
+        total += (x * y);
     }
+    println!("Total = {}", total);
 }
