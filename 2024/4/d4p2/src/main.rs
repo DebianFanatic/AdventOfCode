f5d347f56ee708ebc14a22f0aa4139c3bc4de8a4
Kent West <kent.west@acu.edu>
2025-06-23 10:45:52 -0500

    Just started on d2p2.

--- 2024/4/d4p2/src/main.rs
+++ 2024/4/d4p2/src/main.rs
@@ -1,3 +1,35 @@
+const COMMENTS: &str = r#"
+    Do a word search puzzle, counting all occurrences of two
+    diagonally-centered-intersecting "MAS" sets, whether it's going
+    forward or backwards.
+
+    Advent of Code
+    Day 4, Part 2
+    Kent West - 21.June.2025
+"#;
+
+mod funcs;
+use funcs::*;
+
+const TARGET: &str = "XMAS";
+
 fn main() {
-    println!("Hello, world!");
-}
+    println!("{}", COMMENTS);
+
+    // Read input file.
+    let grid = get_data_grid("src/data.txt");
+print_grid(&grid);
+
+    let lastrow = grid.len();
+    let lastcolumn = grid[0].len();
+    let mut total = 0;
+
+    // Check each cell of the grid for any match in eight directions.
+    for row in 0..lastrow {
+        for col in 0..lastcolumn {
+            let cell = (row, col);
+            total += get_num_of_matches_per_cell(&grid, cell, TARGET);
+        }
+    }
+    println!("Total matches = {}", total);
+} // end of main()
